db406e106157b43615a8a60e4b45b4bde6434fc4
mcupak <mcupak@redhat.com>
2015-04-09 19:39:20 -0400

    fixed several bugs in the client

--- bob-js-client/src/main/webapp/js/bob-api.js
+++ bob-js-client/src/main/webapp/js/bob-api.js
@@ -1,15 +1,15 @@
 // constants
-var timeout = 60000;
+var timeout = 30000;
 var restUrl = "http://beacon-dnastack.rhcloud.com/rest/";
+//var restUrl = "http://localhost:8080/rest/";
 var beaconsUrl = restUrl + "beacons";
 var responsesUrl = restUrl + "responses?";
-var beaconsListUrl = restUrl + "beacons";
 
 function createRequest() {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
         // do nothing
-    } else if (typeof XDomainRequest != "undefined") {
+    } else if (typeof XDomainRequest !== "undefined") {
         // XDomainRequest for IE.
         xhr = new XDomainRequest();
     } else {
@@ -21,11 +21,11 @@ function createRequest() {
 }
 
 function openRequest(xhr, method, url) {
-    if (xhr != null) {
+    if (xhr !== null) {
         if ("withCredentials" in xhr) {
             // XHR for Chrome/Firefox/Opera/Safari.
             xhr.open(method, url, true);
-        } else if (typeof XDomainRequest != "undefined") {
+        } else if (typeof XDomainRequest !== "undefined") {
             xhr.open(method, url);
         }
         xhr.setRequestHeader("Accept", "application/json");
@@ -37,37 +37,32 @@ function openRequest(xhr, method, url) {
 }
 
 function sendRequest(xhr) {
-    if (xhr != null) {
+    if (xhr !== null) {
         xhr.send();
     }
 }
 
-function getBeaconsOtherViews(obj) {
+function getBeaconsDefatult(obj) {
 
-    var resultTable = '<select class="form-control" name="beacon">';
-    resultTable += '<option>All</option>';
-    var arrayLength = obj.length;
-
-    for (var i = 0; i < arrayLength; i++) {
+    var resultTable = '<select class="form-control" name="beacon" id="beacon">';
+    resultTable += '<option value="all">All</option>';
+    for (var i = 0; i < obj.length; i++) {
         if (obj[i] !== null) {
             var tags = (obj[i].aggregator) ? "[aggregator]" : "";
-            resultTable += '<option>' + obj[i].name + ' (' + obj[i].organization + ') ' + tags + '</option>';
+            resultTable += '<option value="' + obj[i].id + '">' + obj[i].name + ' (' + obj[i].organization + ') ' + tags + '</option>';
         }
     }
-
     resultTable += "</select>";
     document.getElementById("beaconlist").innerHTML = resultTable;
 
 }
 
-function getBeaconsGenerateSnippetView(obj){
+function getBeaconsGenerateSnippetView(obj) {
 
     var resultTable = '<select class="selectpicker" multiple data-live-search="true" title="All">';
-    var arrayLength = obj.length;
-    var beaconsInfo = {},
-    beaconOption = "";
+    var beaconsInfo = {}, beaconOption = "";
 
-    for (var i = 0; i < arrayLength; i++) {
+    for (var i = 0; i < obj.length; i++) {
         if (obj[i] !== null) {
             var tags = (obj[i].aggregator) ? "[aggregator]" : "";
             beaconOption = obj[i].name + ' (' + obj[i].organization + ') ' + tags;
@@ -86,14 +81,14 @@ function getBeacons(currentView) {
     var xhr = openRequest(createRequest(), "GET", beaconsUrl);
     xhr.onreadystatechange = function () {
 
-        if (xhr.readyState == 4 && xhr.status == 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             var obj = JSON.parse(xhr.responseText);
 
-            if (currentView == undefined || currentView.localeCompare("generate-snippet") != 0) {
-            	getBeaconsOtherViews(obj);
+            if (currentView === undefined || currentView.localeCompare("generate-snippet") !== 0) {
+                getBeaconsDefatult(obj);
             }
             else {
-            	getBeaconsGenerateSnippetView(obj);
+                getBeaconsGenerateSnippetView(obj);
             }
         }
     };
@@ -108,11 +103,11 @@ function getBeacons(currentView) {
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    return (results === null) ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 function isNull(value) {
-    if (value == null || value === "") {
+    if (value === null || value === "") {
         return true;
     }
     return false;
@@ -129,7 +124,7 @@ function queryBeacon(beacon, chrom, pos, alt, ref) {
 
     xhr.onreadystatechange = function () {
 
-        if (xhr.readyState == 4 && xhr.status == 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
 
             obj = JSON.parse(xhr.responseText);
             var arrayLength = obj.length;
@@ -143,9 +138,9 @@ function queryBeacon(beacon, chrom, pos, alt, ref) {
                 var response = obj[i].response;
                 var responseIndicator = "";
 
-                if (response == null) {
+                if (response === null) {
                     responseIndicator = "<span title='There was a problem obtaining the response for this beacon.' class='label label-warning'>&#x2005;&#x2005;&#x2005;&#x2005;</span>";
-                } else if (response == true) {
+                } else if (response === true) {
                     responseIndicator = "<span class='label label-success'>Yes</span>";
                 } else {
                     responseIndicator = "<span class='label label-danger'>No</span>";
@@ -154,11 +149,11 @@ function queryBeacon(beacon, chrom, pos, alt, ref) {
                 var aggField = obj[i].beacon.aggregator;
                 var aggString = "";
 
-                if (aggField != null && aggField == true) {
+                if (aggField !== null && aggField === true) {
                     aggString = " [aggregator]";
                 }
 
-                if (obj[i].beacon.name == "Beacon of Beacons") {
+                if (obj[i].beacon.name === "Beacon of Beacons") {
                     var line = "<tr style=\"padding-bottom: 10em;\"><td>" + responseIndicator + "</td><td><b>" + obj[i].beacon.name + " (" + obj[i].beacon.organization + ")</b></td></tr>";
                     rows = line + rows; // prepend
                 } else {
@@ -181,10 +176,10 @@ function queryBeacon(beacon, chrom, pos, alt, ref) {
     var urlArr = [];
     urlArr.push(responsesUrl, "chrom=" + chrom, "&pos=" + pos, "&allele=" + alt);
 
-    if (beacon != "all") {
+    if (beacon !== "all") {
         urlArr.push("&beacon=" + beacon);
     }
-    if (ref != "all") {
+    if (ref !== "all") {
         urlArr.push("&ref=" + ref);
     }
     var url = urlArr.join("");
@@ -248,69 +243,69 @@ function printResponses() {
  * helper method for printBeaconsList
  * groups response based on organization
  * returns  { organization-name: [ {'name' : beacon-name, 'aggregator' : value} ] }
-*/
+ */
 function groupResponseByOrganization(response) {
 
-	var groupedResponse = {};
+    var groupedResponse = {};
 
-	for (var i = 0; i < response.length; i++) {
-		var current = response[i];
-		if (groupedResponse[current.organization] == undefined) {
-			var object = [{"name": current.name, "aggregator": current.aggregator}];
-			groupedResponse[current.organization] = object;
-		}
-		else {
-			groupedResponse[current.organization].push( {"name": current.name, "aggregator": current.aggregator} );
-		}
-	}
-	return groupedResponse;
+    for (var i = 0; i < response.length; i++) {
+        var current = response[i];
+        if (groupedResponse[current.organization] === undefined) {
+            var object = [{"name": current.name, "aggregator": current.aggregator}];
+            groupedResponse[current.organization] = object;
+        }
+        else {
+            groupedResponse[current.organization].push({"name": current.name, "aggregator": current.aggregator});
+        }
+    }
+    return groupedResponse;
 }
 
 function printBeaconsList(xhr) {
 
-	var response = groupResponseByOrganization(JSON.parse(xhr.response)),
-	rows = "", responseTable = "", beacons, aggregators, org,
-	sortedOrgs = Object.keys(response).sort();
+    var response = groupResponseByOrganization(JSON.parse(xhr.response)),
+            rows = "", responseTable = "", beacons, aggregators, org,
+            sortedOrgs = Object.keys(response).sort();
 
-	responseTable += "<div><table class=\"table table-condensed row-centered\" style=\"height:50%\"><thead><tr style=\"background-color:#458CC8; color:white;\"><th style=\"text-align:center;\">Organization</th><th style=\"text-align:center;\">Beacon</th><th style=\"text-align:center;\">Aggregator</th></tr></thead><tbody>";
+    responseTable += "<div><table class=\"table table-condensed row-centered\" style=\"height:50%\"><thead><tr style=\"background-color:#458CC8; color:white;\"><th style=\"text-align:center;\">Organization</th><th style=\"text-align:center;\">Beacon</th><th style=\"text-align:center;\">Aggregator</th></tr></thead><tbody>";
 
-	for (var i = 0; i < sortedOrgs.length; i++) {
+    for (var i = 0; i < sortedOrgs.length; i++) {
 
-		org = sortedOrgs[i];
-		rows += "<tr><td style=\"vertical-align: middle\">" + org + "</td>";
-		beacons = "<table class=\"table\">";
-		aggregators = "<table class=\"table\">";
+        org = sortedOrgs[i];
+        rows += "<tr><td style=\"vertical-align: middle\">" + org + "</td>";
+        beacons = "<table class=\"table\">";
+        aggregators = "<table class=\"table\">";
 
-		for(var j = 0; j < response[org].length; j++) {
-			var aggregatorIndicator = response[org][j]["aggregator"] == true ? "<span class=\"label label-success\">Yes</span>" : "<span class=\"label label-danger\">No</span>";
-			beacons += "<tr><td style=\"border: none\">" + response[org][j]["name"] + "</td></tr>";
-			aggregators += "<tr><td style=\"border: none\">" + aggregatorIndicator + "</td></tr>";
-		}
+        for (var j = 0; j < response[org].length; j++) {
+            var aggregatorIndicator = response[org][j]["aggregator"] === true ? "<span class=\"label label-success\">Yes</span>" : "<span class=\"label label-danger\">No</span>";
+            beacons += "<tr><td style=\"border: none\">" + response[org][j]["name"] + "</td></tr>";
+            aggregators += "<tr><td style=\"border: none\">" + aggregatorIndicator + "</td></tr>";
+        }
 
-		beacons += "</table>";
-		aggregators += "</table>";
-		rows += "<td>" + beacons + "</td>";
-		rows += "<td>" + aggregators + "</td></tr>";
-	}
+        beacons += "</table>";
+        aggregators += "</table>";
+        rows += "<td>" + beacons + "</td>";
+        rows += "<td>" + aggregators + "</td></tr>";
+    }
 
-	responseTable += rows;
-	responseTable += "</tbody></table>";
-	document.getElementById("beacon_list_table").innerHTML = responseTable.trim();
+    responseTable += rows;
+    responseTable += "</tbody></table>";
+    document.getElementById("beacon_list_table").innerHTML = responseTable.trim();
 }
 
 function getBeaconsList() {
 
-	var xhr = openRequest(createRequest(), "GET", beaconsListUrl);
+    var xhr = openRequest(createRequest(), "GET", beaconsUrl);
 
-	xhr.onreadystatechange=function() {
-		if (xhr.readyState==4 && xhr.status==200) {
-			printBeaconsList( xhr );
-		}
-	};
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            printBeaconsList(xhr);
+        }
+    };
 
-	xhr.ontimeout = function () {
-		document.getElementById("beacon_list_table").innerHTML = "";
-	};
+    xhr.ontimeout = function () {
+        document.getElementById("beacon_list_table").innerHTML = "";
+    };
 
-	sendRequest(xhr);
+    sendRequest(xhr);
 }
